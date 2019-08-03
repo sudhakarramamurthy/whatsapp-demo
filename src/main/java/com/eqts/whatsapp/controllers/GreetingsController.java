35818f07167e419e461642559776fada4254da23
sudhakar <sudhakarramamurthy@gmail.com>
2019-08-04 00:26:07 +0530

    Working version with receive and post

--- src/main/java/com/eqts/whatsapp/controllers/GreetingsController.java
+++ src/main/java/com/eqts/whatsapp/controllers/GreetingsController.java
@@ -1,8 +1,11 @@
 package com.eqts.whatsapp.controllers;
 
-import java.beans.Beans;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.eqts.whatsapp.receive.WhatsappMsg;
+import com.eqts.whatsapp.send.MsgSentResponse;
+import com.eqts.whatsapp.send.SendMessage;
+import com.eqts.whatsapp.receive.Message;
 
 /**
  *
@@ -29,7 +38,27 @@ public class GreetingsController {
     }
     
     @PostMapping("/messages")
-    void newMessageReceived(@RequestBody Beans beans) {
-      beans.toString();
+    void handleMessage(@RequestBody WhatsappMsg beans) {
+     List<Message> msgs = beans.getMessages();
+     List<Message> filteredmsg = msgs.stream()                        
+             .filter(x -> x.getAuthor().startsWith("919443205969"))
+             .collect(Collectors.toList());
+      filteredmsg.stream().forEach(System.out::println);
+      if (filteredmsg.size() > 0 ) {
+			Message mymsg = filteredmsg.get(0);
+			postResponse(mymsg);
+      }
+     }
+ 
+    private SendMessage postResponse(Message msg) {
+    	SendMessage sMsg = new SendMessage();
+    	System.out.println(msg.getChatId());
+    	sMsg.setChatId(msg.getChatId());
+    	sMsg.setBody("Your Msg has been received by bot!");
+        RestTemplate rt = new RestTemplate();
+        ResponseEntity<MsgSentResponse> response = rt.postForEntity("https://api.chat-api.com/instance56644/sendMessage?token=sphz8698hh2rdaag", sMsg, MsgSentResponse.class);
+        System.out.println(response.getBody().toString());
+    	return null;
     }
+    
 }
---
+++ src/main/java/com/eqts/whatsapp/receive/WhatsappMsg.java
@@ -0,0 +1,66 @@
+package com.eqts.whatsapp.receive;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+"messages",
+"lastMessageNumber"
+})
+public class WhatsappMsg {
+
+@JsonProperty("messages")
+private List<Message> messages = null;
+@JsonProperty("lastMessageNumber")
+private Integer lastMessageNumber;
+@JsonIgnore
+private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+@JsonProperty("messages")
+public List<Message> getMessages() {
+return messages;
+}
+
+@JsonProperty("messages")
+public void setMessages(List<Message> messages) {
+this.messages = messages;
+}
+
+@JsonProperty("lastMessageNumber")
+public Integer getLastMessageNumber() {
+return lastMessageNumber;
+}
+
+@JsonProperty("lastMessageNumber")
+public void setLastMessageNumber(Integer lastMessageNumber) {
+this.lastMessageNumber = lastMessageNumber;
+}
+
+@JsonAnyGetter
+public Map<String, Object> getAdditionalProperties() {
+return this.additionalProperties;
+}
+
+@JsonAnySetter
+public void setAdditionalProperty(String name, Object value) {
+this.additionalProperties.put(name, value);
+}
+
+@Override
+public String toString() {
+return new ToStringBuilder(this).append("messages", messages).append("lastMessageNumber", lastMessageNumber).append("additionalProperties", additionalProperties).toString();
+}
+
+}
